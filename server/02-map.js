4a0b47853f226a607beb9573fa1c4cb2f90739c2
Daniel Romero <daanmaro99june@gmail.com>
2022-05-30 19:03:47 -0500

    map js

--- server/02-map.js
+++ server/02-map.js
@@ -5,3 +5,21 @@ const map1 = array1.map(x => x * 2);
 
 console.log(map1);
 // expected output: Array [2, 8, 18, 32]
+
+
+
+
+const letters = ['a', 'b', 'c'];
+
+//Transformación con un for.
+
+// const newArray = [];
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+
+const newArray = letters.map(item => item + '++');
+
+console.log('orginal', letters);
+console.log('new', newArray)
