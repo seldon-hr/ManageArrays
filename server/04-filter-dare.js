2f1d4921398a1056a7993a78d2a728050ffc841f
Daniel Romero <daanmaro99june@gmail.com>
2022-06-02 19:56:15 -0500

    filter practice.2

---
+++ server/04-filter-dare.js
@@ -0,0 +1,10 @@
+const array = ['amor', 'sol', 'piedra', 'día'];
+
+// const array = words.filter(item => item.length >= 4);
+// console.log('NEW ARRAY', array);
+
+function solution (array) {
+  return array.filter(item => item.length >= 4);
+}
+
+solution();
