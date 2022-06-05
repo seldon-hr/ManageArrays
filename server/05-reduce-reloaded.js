b117415317f74f1143d6abc632f676076bc8c916
Daniel Romero <daanmaro99june@gmail.com>
2022-06-04 22:42:56 -0500

    reduce with objec using some objectst

--- server/05-reduce-reloaded.js
+++ server/05-reduce-reloaded.js
@@ -22,3 +22,45 @@ Output
     2:1
 }
 */
+
+const data = [
+    {
+        name: "Nicolas",
+        level: "low",
+      },
+      {
+        name: "Andrea",
+        level: "medium",
+      },
+      {
+        name: "Zulema",
+        level: "hight",
+      },
+      {
+        name: "Santiago",
+        level: "low",
+      },
+      {
+        name: "Valentina",
+        level: "medium",
+      },
+      {
+        name: "Lucia",
+        level: "hight",
+      },
+];
+
+const rta1 = data
+.map( item => item.level)
+.reduce((obj, item) => {
+    if (!obj[item]) {  
+        obj[item] = 1; 
+    } else {
+        obj[item] = obj[item] + 1; 
+    }
+    return obj;
+}, {})
+
+console.log(rta1)
+
+//Concatenando métodos y pasando la salida de uno a ser la entrada de otro.
