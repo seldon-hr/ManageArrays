58d4f2b6aeacf263b3703bea0f866299d99095d0
Daniel Romero <daanmaro99june@gmail.com>
2022-06-08 18:29:01 -0500

    find

--- server/10-find.js
+++ server/10-find.js
@@ -13,3 +13,34 @@ console.log('for', rta);
 
 const rta2 = numbers.find(item => item === 60)
 console.log('find', rta2);
+
+
+const products = [
+    {
+      name: "Pizza",
+      price: 12,
+      id: '🍕'
+    },
+    {
+      name: "Burger",
+      price: 23,
+      id: '🍔'
+    },
+    {
+      name: "Hot dog",
+      price: 34,
+      id: '🌭'
+    },
+    {
+      name: "Hot cakes",
+      price: 355,
+      id: '🥞'
+    },
+  ];
+  
+
+  const rta3 = products.find(item => item.id === '🍔')
+  console.log('find', rta3);
+//findIndex devuelve el valor de posición a diferencia de find, que devuelve el dato (objeto, parte del array)
+  const rta4 = products.findIndex(item => item.id === '🍔')
+  console.log('findIndex', rta4);
