4f68d6ed5ea554a97d1f27ac346acab915e33e6d
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-15 17:01:22 -0500

    Coerción de tipos

--- server/!!Sign.js
+++ server/!!Sign.js
@@ -1,1 +1,11 @@
+//We have some aspects; what will their answers?
 
+!!null;
+!!'';
+!!1;
+
+// This operator !! means double negation.
+// The key is coerción de tipos.
+!!null; // false, because null is a falsy value, so if we negative two times we have false
+!!'';  //'' false, '' is a falsy value, false
+!!1; // the value 1 is a truthy value, dobule negativon we got true.
