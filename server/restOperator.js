da3687c2fa58cad656efe54bf75432b6bcba98ea
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-16 19:50:17 -0500

    Type of date with rest operartor
    Using ... to modify

--- server/restOperator.js
+++ server/restOperator.js
@@ -6,7 +6,11 @@ getAge(21);
 
 //What's going to print?
 /*
+Cuando usamos la sintaxis de '...' en los parámetros de una función (ES6: Rest Operator) convertimos a dicho parámetro a un 
+arreglo.
 
+Entoncees es tentador marcar la opción B: 'array' pero este es un error común. En JS no existe el tipo de dato array, para
+tipos no primitivos el lenguaje los evalua como object. Por ese motivo la respuesta correcta es que es de tipo 'object'.
 
 
 */
