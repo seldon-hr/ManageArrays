120d2797d185ebafa5b5a7507b556d006094ca34
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-17 21:06:36 -0500

    Update desconstruccion.js

--- server/desconstruccion.js
+++ server/desconstruccion.js
@@ -3,7 +3,18 @@ const { 3: pera } = frutas;
 console.log(pera);
 
 /*
+Respuesta Correcta: D) Pera
+Para usar la desestructuración en arreglos es importante tener en cuenta los índices de los elementos. Por ello para acceder a Pera en el arreglo frutas hariamos algo como:
 
+const [, , , pera] = frutas;
+
+Donde cada , representa el salto de un índice del arreglo.
+
+Para una sistaxis mas breve podemos usar esto:
+
+const { 3:pera } = frutas;
+
+Donde el 3 representa las posiciones que deseamos saltar. Nota que aunque frutas sea un arreglo usamos {} para la desestructuración
 
 
 
