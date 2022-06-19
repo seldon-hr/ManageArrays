4af3d71ade98a8ab60b1b56b59e9e6ddd23b5156
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-18 21:34:29 -0500

    Update spreadOperatorStrings.js

--- server/spreadOperatorStrings.js
+++ server/spreadOperatorStrings.js
@@ -2,4 +2,5 @@
 
 console.log([...'Oscar']);
 
-
+// Respuesta Correcta: A) ["O", "s", "c", "a", "r"]
+// Un string es un elemento iterable en JavaScript, por ende es posible usar el spread operator directamente obteniendo la propagación de la cadena letra por letra.
