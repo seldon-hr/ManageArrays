c8a3c700456ed7e00ae6b9a9daa353b4a44c1f0c
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-19 21:12:18 -0500

    Use setInterval.js

--- server/setInterval.js
+++ server/setInterval.js
@@ -3,3 +3,5 @@
 setInterval(() => console.log('Hi'), 1000);
 
 
+// Respuesta Correcta: C) Hi cada segundo.
+// La función setInterval es una Web Api que recibe un intervalo en milisegundos, e imprime el cuerpo de la función en dicho intervalo.
