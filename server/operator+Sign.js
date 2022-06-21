311212abe0b40539a3fb00074d5adab27a922417
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-20 20:57:48 -0500

    Update operator+Sign.js

--- server/operator+Sign.js
+++ server/operator+Sign.js
@@ -1,3 +1,7 @@
 console.log(typeof ([] + []));
 
 
+// Respuesta Correcta: D) string
+// El operador + por lo general intentará realizar una concateneción, en este caso, el interprete de JavaScript, por coerción de tipos intentará convertir los arreglos a cadenas de texto, haciendo algo como esto aunque no lo veamos:
+
+// console.log(typeof ([].toString() + [].toString())); //string console.log(typeof ("" + "")); console.log(typeof ("")); //string
