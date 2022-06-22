52a3871578c6c171dbdb3958b7006b20b4084c43
Daniel Romero <52944651+seldon-hr@users.noreply.github.com>
2022-06-21 21:34:30 -0500

    Update typeof.js

--- server/typeof.js
+++ server/typeof.js
@@ -2,4 +2,5 @@ console.log(typeof typeof 1);
 
 // ¿Cuál sería el resultado?
 
-// String, Una cadena dobble, así que primero preguntamos acerca
+// String, Una cadena dobble, así que primero preguntamos acerca de typepf 1, esto nos devuelve 'number', pero volvemos a hacer un typeof de number, esto es una cadena
+// un string.
